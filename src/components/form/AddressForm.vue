6482ac672d7e2f73f09241b1b7c958a2750dfda5
Rafael Medina <rafael.medina46005@ucaldas.edu.co>
2025-06-11 00:27:50 -0500

    create address button

--- src/components/form/AddressForm.vue
+++ src/components/form/AddressForm.vue
@@ -1,11 +1,21 @@
 <template>
+    
   <div class="grid grid-cols-1 md:grid-cols-2 gap-4 bg-white p-6 rounded shadow">
     <!-- Mapa -->
+
+       
+
     <div ref="mapContainer" class="h-64 md:h-full rounded border" />
 
     <!-- Formulario -->
     <form @submit.prevent="emitSubmit" class="space-y-4">
       <h2 class="text-xl font-semibold">FBC - Address</h2>
+      <button
+        @click="gotoCreate"
+        class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded absolute right-14 top-24"
+      >
+        Create Address
+      </button>   
       <div>
         <label class="block mb-1 font-medium">Street</label>
         <input
@@ -51,6 +61,11 @@ import type { Address } from '@/models/Address'
 const props = defineProps<{
   modelValue: Address
 }>()
+const router = useRouter()
+
+const gotoCreate = () => {
+    router.push('/address/create')
+}
 
 const emit = defineEmits<{
   (e: 'update:modelValue', val: Address): void
@@ -104,6 +119,10 @@ watch(form, (val) => emit('update:modelValue', val), { deep: true })
 const emitSubmit = () => {
   emit('submit', form.value)
 }
+import { useRouter } from 'vue-router'
+
+
+
 </script>
 
 <style scoped>
